b0a775fb429b512167fe3c7b91adfe877a9fd842
Suryalakshminarayana Dhulipudi <suryalakshminarayana.d@gmail.com>
2021-04-11 11:41:08 +0530

    Initial commit

---
+++ python/learning/armstrong_number.py
@@ -0,0 +1,17 @@
+given_number = int(input("Enter a number:"))
+
+def armstrong(given_number):
+    sum = 0
+    n  = len(str(given_number))
+    while given_number >0:
+        que = given_number%10
+        print(que)
+        sum = sum + (que**int(n))
+        given_number = given_number//10
+    return sum
+
+
+if armstrong(given_number) == given_number:
+    print("%d is an armstrong number" %(given_number))
+else:
+    print(given_number, "is not an armstrong number")
