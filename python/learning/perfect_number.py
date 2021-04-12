19d2957bf651ff7cfee3e4fe0b522d5367a41db6
Suryalakshminarayana Dhulipudi <suryalakshminarayana.d@gmail.com>
2021-04-12 16:47:43 +0530

    Initial commit

---
+++ python/learning/perfect_number.py
@@ -0,0 +1,35 @@
+#Print perfect numbers
+start = 0
+end = 500
+
+
+def perfect_numbers(start,end):
+    for i in range(start, end+1):
+        temp = 0
+        for x in range(1,i):
+            if(i%x == 0):
+                temp = temp + x
+        if (temp == i) and i!=0:
+            print(i)
+            
+perfect_numbers(start,end)
+
+
+#Check given number is perfect or not
+n = int(input("Enter a number:"))
+
+
+def perfect_numbers(n):
+    temp = 0
+    for x in range(1,n):
+        if(n%x == 0):
+            temp = temp + x
+    if (temp == n) and n!=0:
+        print(n,"is perfect number")
+          
+    else:
+        print(n,"is not perfect number")
+perfect_numbers(n)
+
+
+
