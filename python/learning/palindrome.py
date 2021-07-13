46d7b0ef234de8381bd230a43f56ad4e6702bed3
Suryadhulipudi <suryalakshminarayana.d@gmail.com>
2021-07-13 20:45:34 +0530

    Added palindrome.py ipaddress.py

--- python/learning/palindrome.py
+++ python/learning/palindrome.py
@@ -23,3 +23,17 @@ def palindrome(start,end):
             print(i,"is not palindrome")
             
 palindrome(start,end)
+
+#Method to print without function
+
+num=int(input("Enter a number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
