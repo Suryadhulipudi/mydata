20c662d1cfa6f6b2864ea71f3ed7f30cd581f097
Suryalakshminarayana Dhulipudi <suryalakshminarayana.d@gmail.com>
2023-07-19 14:59:46 +0530

    Update parse_ip_address.py

--- python/learning/parse_ip_address.py
+++ python/learning/parse_ip_address.py
@@ -1,4 +1,37 @@
-#Method-1
+#Method-1 Extract Ip addresses ane bytes transferred
+import re
+
+def extract_ip_bytes(log_file):
+    ip_bytes = {}
+
+    with open(log_file, 'r') as file:
+        for line in file:
+            # Use regular expressions to find IP addresses and bytes transferred
+            matches = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?(\d+)$', line)
+
+            for match in matches:
+                ip = match[0]
+                bytes_transferred = int(match[1])
+
+                if ip in ip_bytes:
+                    ip_bytes[ip] += bytes_transferred
+                else:
+                    ip_bytes[ip] = bytes_transferred
+
+    return ip_bytes
+
+# Example log file path
+log_file = 'example.log'
+
+# Extract IP addresses and bytes transferred from log file
+ip_bytes = extract_ip_bytes(log_file)
+
+# Print the extracted IP addresses and corresponding bytes transferred
+print("IP Addresses and Bytes Transferred:")
+for ip, bytes_transferred in ip_bytes.items():
+    print(f"IP: {ip}, Bytes Transferred: {bytes_transferred}")
+
+#Method-2
 import re
 
 def extract_ip_addresses(log_file):
@@ -25,7 +58,7 @@ for ip_address in ip_addresses:
 
 
 
-#Method-2
+#Method-3
 # importing the module
 import re
   
