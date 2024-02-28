a7f72dfbb48e963f5ce866bcc6e4ce149a275683
Suryalakshminarayana Dhulipudi <suryalakshminarayana.d@gmail.com>
2024-02-28 14:10:05 +0530

    Update sort_numbers.py

--- python/learning/sort_numbers.py
+++ python/learning/sort_numbers.py
@@ -14,3 +14,24 @@ for i in range (Number):
             NumList[j] = temp
 
 print("Element After Sorting List in Ascending Order is : ", NumList)
+
+# Method-2
+def sort_numbers(numbers):
+    sorted_numbers = sorted(numbers)
+    return sorted_numbers
+
+# Example usage
+numbers = [5, 2, 8, 1, 9]
+sorted_numbers = sort_numbers(numbers)
+print("Sorted numbers:", sorted_numbers)
+
+# Method3
+def sort_numbers(numbers):
+    numbers.sort()
+    return numbers
+
+# Example usage
+numbers = [5, 2, 8, 1, 9]
+sorted_numbers = sort_numbers(numbers)
+print("Sorted numbers:", sorted_numbers)
+
